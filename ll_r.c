ca84ee938d06baa9bba300935c0bb07890f9cfde
YASHBISHT20 <96107895+YASHBISHT20@users.noreply.github.com>
2021-12-14 13:59:04 +0530

    Update ll_r.c

--- ll_r.c
+++ ll_r.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
-#inlude<stdlib.h>
+#include<stdlib.h>
 struct node{
-int info;
+int data;
 struct node *next;
 };
 typedef struct node node;
 void add( node ** head, int data){
 node *temp =(node*)malloc(sizeof(node));
-if(temp==NULL)printf("\memory not assigned\n");
+if(temp==NULL)printf("\nmemory not assigned\n");
 
-temp->info=data;
+temp->data=data;
 temp->next=NULL;
 if(*head==NULL){
 *head=temp;
@@ -18,9 +18,37 @@ else{
 node*n=*head;
 while(n->next!=NULL){
 n=n->next;
-
+}
+n->next=temp;
+temp->next=NULL;
 }
 }
 
+void display(node* head){
+if(head==NULL){
+printf("list is empty:\n");
+}
+else{
+while(head!=NULL){
+printf(" %d ",head->data);
+head=head->next;
+}
+}
+}
+
+
+int main(){
+int x=1,choice=1;node* head=NULL;
+while(choice){
+printf("enter value to be stored:\t");
+scanf("%d",&x);
+add(&head,x);
+printf("\nDo you want to contiue\n1.YES\n0.NO\t");
+scanf("%d",&choice);
+}
+printf("linked list before: \n");
+display(head);
+
+//reverse(&head);
 
 }
